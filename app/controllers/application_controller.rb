b610ab87c893278e66c5ce56c0eba7c63c2dacd0
Đăng LV <danglv10688@gmail.com>
2017-11-14 11:05:51 +0700

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  protected
+
+  def authenticate_user!(options={})
+    if user_signed_in?
+      super(options)
+    else
+      return if devise_controller?
+      redirect_to new_user_session_path
+    end
+  end
 end
