9fb6a806fdb9dd0da6f7b3beaa63cc3cb170c15c
Đăng LV <danglv10688@gmail.com>
2017-11-15 09:15:01 +0700

    setup admin & base admin for authorization

--- app/models/role.rb
+++ app/models/role.rb
@@ -19,8 +19,7 @@
 
 # Role
 class Role < ApplicationRecord
-  has_many :users_roles, dependent: :destroy
-  has_many :users, through: :users_roles
+  has_and_belongs_to_many :users, join_table: :users_roles
 
   belongs_to :resource,
              polymorphic: true,
