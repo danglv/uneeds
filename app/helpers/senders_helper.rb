c9591263cf0d52e0a0ac86998ffb3c13a9ae7e3e
Linh Nguyen <vanlinha3k35pbc@gmail.com>
2017-12-08 19:15:34 +0700

    Fix bug can not create transfer

---
+++ app/helpers/senders_helper.rb
@@ -0,0 +1,7 @@
+module SendersHelper
+  def country_options
+    Sender.countries.keys.map do |country|
+      [I18n.t("dictionary.#{country}"), country]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207072801) do
+ActiveRecord::Schema.define(version: 20171208110511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20171207072801) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "currency"
     t.index ["user_id"], name: "index_senders_on_user_id"
   end
 
