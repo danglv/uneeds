88c1d155a51a3acf87c159ac835ffed4bb812641
Linh Nguyen <vanlinha3k35pbc@gmail.com>
2018-01-17 18:35:18 +0700

    Change layout follow feedback

--- app/controllers/admin/transfers_controller.rb
+++ app/controllers/admin/transfers_controller.rb
@@ -1,7 +1,7 @@
 # transfer
 class Admin::TransfersController < Admin::BaseController
   def index
-    @transfers_below = Transfer.below
-    @transfers_above = Transfer.above
+    @transfers_below = Transfer.below.decorate
+    @transfers_above = Transfer.above.decorate
   end
 end
